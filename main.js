8e203d91f7e1e4e61b9a8fe4fdae782a48ebfd58
Marcos Baravalle <marcosabaravalle@gmail.com>
2022-05-07 14:35:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,112 @@
+"use strict";
+
+// nav bar logic
+const openBtn = document.getElementById("hamburger-btn");
+const closeBtn = document.getElementById("close-btn");
+const navContainer = document.querySelector(".nav-container");
+
+const navLink = document.querySelector("#nav-item");
+const navLink1 = document.querySelector("#nav-item-1");
+const navLink2 = document.querySelector("#nav-item-2");
+const navLink3 = document.querySelector("#nav-item-3");
+
+openBtn.addEventListener("click", () =>
+  navContainer.classList.toggle("active")
+);
+closeBtn.addEventListener("click", () =>
+  navContainer.classList.toggle("active")
+);
+
+navLink.addEventListener("click", () =>
+  navContainer.classList.toggle("active")
+);
+navLink1.addEventListener("click", () =>
+  navContainer.classList.toggle("active")
+);
+navLink2.addEventListener("click", () =>
+  navContainer.classList.toggle("active")
+);
+
+navLink3.addEventListener("click", () =>
+  navContainer.classList.toggle("active")
+);
+
+// let i = 0;
+// let txt = "Marcos Baravalle"; /* The text */
+
+// let speed = 50; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+// typeWriter();
+
+// List of sentences
+var _CONTENT = ["Front end web developer", "Economist", "Musician"];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() {
+  // Get substring with 1 characater added
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX++;
+
+  // If full sentence has been displayed then start to delete the sentence after some time
+  if (text === _CONTENT[_PART]) {
+    // Hide the cursor
+    _CURSOR.style.display = "none";
+
+    clearInterval(_INTERVAL_VAL);
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Delete, 50);
+    }, 1000);
+  }
+}
+
+// Implements deleting effect
+function Delete() {
+  // Get substring with 1 characater deleted
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX--;
+
+  // If sentence has been deleted then start to display the next sentence
+  if (text === "") {
+    clearInterval(_INTERVAL_VAL);
+
+    // If current sentence was last then display the first one, else move to the next
+    if (_PART == _CONTENT.length - 1) _PART = 0;
+    else _PART++;
+
+    _PART_INDEX = 0;
+
+    // Start to display the next sentence after some time
+    setTimeout(function () {
+      _CURSOR.style.display = "inline-block";
+      _INTERVAL_VAL = setInterval(Type, 100);
+    }, 200);
+  }
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
