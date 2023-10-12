9e29325be86c587e6f4dd12cf096084f49a3377a
Marcos Baravalle <marcosabaravalle@gmail.com>
2023-10-11 21:50:02 -0300

    fix typo

--- app/footer/Footer.tsx
+++ app/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="w-11/12 m-auto text-center py-12">
       <p>
-        Made with lots of love my Marcos <br /> ໒꒰ྀི മ ̫ മ꒱ྀིა
+        Made with lots of love by Marcos <br /> ໒꒰ྀི മ ̫ മ꒱ྀིა
       </p>
     </div>
   );
